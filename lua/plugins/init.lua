66ceb05a5402a2409356a66bd2f43d3b991e67f8
Michał Czyż <mike@c2yz.com>
2024-04-14 18:03:08 +0200

    weird error

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -122,14 +122,14 @@ return {
     end,
   },
 
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function()
-      local M = require "plugins.configs.cmp"
-      table.insert(M.sources, {name = "crates"})
-      return M
-    end,
-  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   opts = function()
+  --     local M = require "plugins.configs.cmp"
+  --     table.insert(M.sources, {name = "crates"})
+  --     return M
+  --   end,
+  -- },
 
   {
     "nanotee/zoxide.vim",
