0dc06a4a047fb69bfbf6ebed4a29c7fe3a654503
Michael Czyż <mike@c2yz.com>
2024-07-03 09:16:29 +0200

    fixed nvim

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,12 +1,8 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-
-    javascript = { "prettier" },
-    css = { "prettier" },
-    html = { "prettier" },
-
-    sh = { "shfmt" },
+    -- css = { "prettier" },
+    -- html = { "prettier" },
   },
 
   -- format_on_save = {
